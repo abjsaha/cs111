67104303699343a048fba38df997c7793f5fa1cf
abjsaha <abhijoysaha25@gmail.com>
2015-05-09 18:29:59 -0700

    closing hella shitv6

--- lab1-Skeleton/execute-command.c
+++ lab1-Skeleton/execute-command.c
@@ -215,7 +215,7 @@ void execute_this(command_t com)
 				close(fd[1]);
 				dup2(fd[0],0);
 				execute_this(com->u.command[1]);
-				close(fd[0]);
+				//close(fd[0]);
 				exit(com->u.command[1]->status);
 			}
 			else
@@ -229,7 +229,7 @@ void execute_this(command_t com)
 					close(fd[0]);
 					dup2(fd[1],1);
 					execute_this(com->u.command[0]);
-					close(fd[1]);
+					//close(fd[1]);
 					exit(com->u.command[1]->status);
 				}
 				else
